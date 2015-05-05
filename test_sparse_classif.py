d280161c2c9b65580a3f6f95cd89c6a261a05a2f
mfauvel <mathieu.fauvel@ensat.fr>
2015-05-05 10:44:51 +0200

    modification  of the test function

--- test_sparse_classif.py
+++ test_sparse_classif.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 import npfs as npfs
 import scipy as sp
+import time
 
 n = 400 # Number of samples
 d = 200 # Number of dimension
@@ -16,7 +16,6 @@ y[t>sp.mean(t)]=2
 
 t += noise*sp.mean(t)*sp.random.randn(n) # add some noise
 
-
  
 model = npfs.GMM()
 model.learn_gmm(x, y)
@@ -25,16 +24,37 @@ yp.shape=y.shape
 t = sp.where(yp==y)[0]
 print float(t.size)/y.size
 
-# 5-CV
+# 5-CV
+ts= time.time()
 ids,error=model.forward_selection(x, y,delta=1.5,v=5)
+ids.sort()
+yp = model.predict_gmm(x,ids=ids)[0]
+j = sp.where(yp.ravel()==y.ravel())[0]
+OA = (j.size*100.0)/y.size
 print 'Results for 5-CV'
+print 'Proccesing time', time.time()-ts
 print ids
 print error
+print OA
 print var
 
-# LOO CV
-ids,error=model.forward_selection(x, y,delta=1.5,v=None)
-print 'Results for LOO-CV'
+# JM
+ts= time.time()
+ids,error=model.forward_selection_JM(x, y,delta=1.5,maxvar=3)
+yp = model.predict_gmm(x,ids=ids)[0]
+j = sp.where(yp.ravel()==y.ravel())[0]
+OA = (j.size*100.0)/y.size
+print 'Results for JM'
+print 'Proccesing time', time.time()-ts
 print ids
 print error
+print OA
 print var
+
+
+# # LOO CV
+# ids,error=model.forward_selection(x, y,delta=1.5,v=None)
+# print 'Results for LOO-CV'
+# print ids
+# print error
+# print var
